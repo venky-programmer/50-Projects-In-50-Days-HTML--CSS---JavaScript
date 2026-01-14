c49046f296ae3629be772601c0ecd8df61f38489
venky-programmer <venkateshdp333@gmail.com>
2026-01-14 13:30:00 +0530

    Live User Filter

---
+++ 43 Day 42 - Live User Filter/script.js
@@ -0,0 +1,40 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+const listItems = [];
+
+filter.addEventListener("input", (e) => filterData(e.target.value));
+
+async function getData() {
+  const response = await fetch("https://randomuser.me/api?results=50");
+  const { results } = await response.json();
+  result.innerHTML = "";
+
+  results.forEach((user) => {
+    const li = document.createElement("li");
+
+    listItems.push(li);
+
+    li.innerHTML = `
+    <img
+        src="${user.picture.large}" alt="${user.name.first}"/>
+    <div class="user-info">
+        <h4>${user.name.title} ${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+    </div>
+    `;
+
+    result.appendChild(li);
+  });
+}
+
+getData();
+
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+}
